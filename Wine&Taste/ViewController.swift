6616f897391f78a56771d38cb7b7296a3a6696d6
黃暉德 <WadeHuang@wadehunagmacbookair.local>
2018-10-07 21:18:59 +0800

    Initial Commit

---
+++ Wine&Taste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wine&Taste
+//
+//  Created by 黃暉德 on 2018/10/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
